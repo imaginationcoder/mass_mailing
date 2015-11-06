f12d7222897fe35fa9ad709b502638dec2fede2c
imaginationcoder <labs@maisasolutions.com>
2015-11-06 21:56:20 +0530

    fixed the issue

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,4 +1,9 @@
-class Notifier < ApplicationMailer
+class Notifier < ActionMailer::Base
+  default from: '"SamePinch" < hello@samepinch.co >'
+  default from_name: "SamePinch"
+
+  layout 'mailer'
+
   def welcome_mail(emails, subject)
     mail(to: emails, subject: subject)
   end
